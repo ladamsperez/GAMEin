1835beec7de53ce81f7b8060886c2bbbb5d4b49a
ladamsperez <ladamsperezwork@gmail.com>
2021-09-29 19:00:12 -0400

    trying to deploy changes

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,7 +1,6 @@
 """backend URL Configuration
-
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.2/topics/http/urls/
+    https://docs.djangoproject.com/en/3.1/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -13,17 +12,17 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-# backend/urls.py
-
 from django.contrib import admin
-from django.urls import path, include                   # add this
-from rest_framework import routers                      # add this
-from todo import views                                  # add this
+from django.urls import path, re_path, include
+from django.views.generic import TemplateView
+from rest_framework import routers
+from todo import views
 
-router = routers.DefaultRouter()                        # add this
-router.register(r'todos', views.TodoView, 'todo')       # add this
+router = routers.DefaultRouter()
+router.register(r'todos', views.TodoView, 'todo')
 
 urlpatterns = [
     path('admin/', admin.site.urls),         
-    path('api/', include(router.urls))                   # add this
+    path('api/', include(router.urls)),
+    re_path('.*', TemplateView.as_view(template_name='index.html')),
 ]
